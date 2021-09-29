9b82b8ce5c49b40ec5645ba419ac2bee1048bc69
Anupam Kushwaha <anupamkushwaha1426@gmail.com>
2021-09-29 14:25:23 +0530

    Added class, objects, and refrences

--- src/ReferenceAndValueTypes.java
+++ src/ReferenceAndValueTypes.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class ReferenceAndValueTypes {
+public class ReferenceAndValueTypes {
+    public static void main(String[] args) {
+        Person anu = new Person();
+        anu.setName("Anuapm");
+        anu.setAge(22);
+        celebrateBirthday(anu);
+        System.out.println(anu.getName() +"'s age after recent birthday has become "+ anu.getAge());
+
+    }
+
+    private static void celebrateBirthday(Person p) {
+        p.setAge(p.getAge() + 1);
+    }
 }
